647b1a4d16238d5cf626a57278d9ae65e44da255
LuisDio <luis.diolindo@gmail.com>
2019-11-19 23:29:34 +0300

    search box filter event added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const state = {
-    robots: robots,
-    searchField: ''
-}
 
 class App extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange =(event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
     render() {
+        const filteredRobot = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
         return(
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobot}/>
             </div>
         );
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class SearchBox extends Component {
+    constructor(props) {
+        super()
+
+    }
 
     render () {
         return(
             <div className="pa2">
-                <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+                <input onChange={this.props.searchChange} className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
             </div>
     
         );
