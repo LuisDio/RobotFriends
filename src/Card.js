c4f29c6999de3b3d84eb13ea91b97a763ae134af
LuisDio <luis.diolindo@gmail.com>
2019-11-19 16:14:32 +0300

    templating

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+
     render() {
+        
         return(
             <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
-                <img alt='Robot' src='https://robohash.org/test?200x200' />
+                <img alt='Robot' src={`https://robohash.org/${this.props.id}?200x200`} />
                 <div>
                     <h2>{this.props.name}</h2>
                     <p>{this.props.email}</p>
