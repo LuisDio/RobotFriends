a0459bf899ff27c5cb6c7151cb257ae915893367
LuisDio <luis.diolindo@gmail.com>
2019-11-19 16:48:35 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <div>
-        {/* givin properties to card in the parent
-            and then using it to child component as props
-        */}
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
-, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
