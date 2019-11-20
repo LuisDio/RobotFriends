f4172869b73a097db3ab3104715e690f659f8f43
LuisDio <luis.diolindo@gmail.com>
2019-11-20 14:13:28 +0300

    added loading to fetch

--- src/App.js
+++ src/App.js
@@ -29,13 +29,17 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
 
-        return(
-            <div className="tc">
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobot}/>
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading</h1>
+        } else {
+            return(
+                <div className="tc">
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobot}/>
+                </div>
+            );
+        }
     }
 }
 
